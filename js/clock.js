ce6a30da808cb0ea03bbd2910c678e84a3b903f9
J750N <jasonkumarr@gmail.com>
2024-11-13 14:59:31 +0000

    test

---
+++ js/clock.js
@@ -0,0 +1,11 @@
+(function(){ 
+console.info("Hello JS"); 
+})();
+
+const myNode = document.createElement("div");
+digitalClock.appendChild(myNode);
+myNode.innerHTML = "Welcome";
+
+
+const myDate = new Date();
+console.dir(myDate);
---
+++ js/staff.js
@@ -0,0 +1,10 @@
+var myObject = { name : "Martin", email: "myemail@company.com"};
+console.dir(myObject);
+
+fetch(url)
+  .then(response => {
+    // Handle the response here
+  })
+  .catch(error => {
+    // Handle errors here
+  });
