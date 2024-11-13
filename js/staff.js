35e80c838e028cdae1897b799731e8e15223837a
J750N <jasonkumarr@gmail.com>
2024-11-13 15:52:20 +0000

    new

--- js/staff.js
+++ js/staff.js
@@ -1,10 +1,29 @@
-var myObject = { name : "Martin", email: "myemail@company.com"};
-console.dir(myObject);
+(() => {
+  //start
+  console.info("READY");
 
-fetch(url)
-  .then(response => {
-    // Handle the response here
+fetch("data/staff.json")
+  .then((response) => response.json())
+  .then(data => {
+    // Work with the parsed data
+    console.dir(data)
+    data.forEach((item) => {
+      const row = document.createElement("tr");
+      const nameCell = document.createElement("td");
+      const emailCell = document.createElement("td");
+
+    nameCell.textContent = `${item.first_name} ${item.last_name}`;
+    emailCell.textContent = item.email;
+
+    row.appendChild(nameCell);
+    row.appendChild(emailCell);
+    staffTable.appendChild(row);
+
+
+      // ...
+    });
   })
   .catch(error => {
     // Handle errors here
   });
+})();
